96886c8d5486ff364acad095edd2a020485a0cc2
Lukie Kang <lukiek+f@gmail.com>
2019-05-29 20:20:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,12 @@
 
 def key_for_min_value(name_hash)
   min = nil
+  min_name = nil
   name_hash.each do | name_key, name_value |
     if min ==nil
-      min = name_key
+      min_name = name_key
     elsif name_value < min
-      min = name_key
+      min_name = name_key
     end
   end
   min
